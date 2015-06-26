b2e844d8b1de1e1a217b225f81150ef2a40652c4
CallMeNono <327336383@qq.com>
2015-06-27 00:34:22 +0800

    fucking

---
+++ docsearch/docsearcjTests/SuperSearchTests.m
@@ -0,0 +1,34 @@
+//
+//  SuperSearchTests.m
+//  SuperSearchTests
+//
+//  Created by zhaojianguo on 14-3-6.
+//  Copyright (c) 2014年 zhaojianguo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SuperSearchTests : XCTestCase
+
+@end
+
+@implementation SuperSearchTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
